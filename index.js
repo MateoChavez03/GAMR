668d8a59ac10a6df10311cb77dd8a9eb94762f50
Mateo Chavez <mateochavez424@gmail.com>
2022-04-25 19:19:29 -0300

    primer desafio entregable

--- index.js
+++ index.js
@@ -1,12 +1,65 @@
-let grupoFamiliar = `El grupo familiar esta conformado por:\n`;
-let nombre = prompt("Ingrese el primer miembro de su grupo familiar:");
-let ingresos = parseFloat(prompt("Ingrese el sueldo del primer miembro:"));
+let entrada = parseFloat(prompt("Seleccione una de las siguientes opciones: \n1) Agregar Horizzon forbidden west al carrito \n2) Agregar Halo Infinite al carrito \n3) Vaciar carrito \n4) Consultar carrito \n5) Finalizar compra"));
+let carrito = 0
 
-while (nombre != "ESC") {
-    alert(`El sueldo de ${nombre} es de $${ingresos}`);
-    grupoFamiliar += `${nombre} con un sueldo de: $${ingresos}\n`;
-    nombre = prompt("Ingrese el siguiente miembro:");
-    ingresos = parseFloat(prompt("Ingrese el sueldo del siguiente miembro:"));
+function agregarHorizon() {
+    carrito += 2600;
+    alert("Horizon forbidden west fue agregado correctamente al carrito");
 }
 
-alert(grupoFamiliar);
+function agregarHalo() {
+    carrito += 3000;
+    alert("Halo Infinite fue agregado correctamente al carrito");
+}
+
+function vaciarCarrito() {
+    carrito = 0;
+    alert("El carrito fue vaciado correctamente");
+}
+
+function consultarCarrito() {
+    switch (carrito) {
+        case 0:
+            alert("El carrito esta vacio");
+            break;
+        case 2600:
+            alert(`Horizon forbidden west se encuentra en el carrito, el valor del carrito es de $${carrito}`);
+            break;
+        case 3000:
+            alert(`Halo Infinite se encuentra en el carrito, el valor del carrito es de $${carrito}`);
+            break;
+        case 5600:
+            alert(`Horizon forbidden west y Halo Infinite se encuentran en el carrito, el valor del carrito es de $${carrito}`);
+            break;
+        default:
+            alert(`Ocurrio un error con el carrito, solo puede agregar una copia de cada juego`);
+            vaciarCarrito()
+            break;
+    }
+}
+
+while (entrada != 5) {
+    switch (entrada) {
+        case 1:
+            agregarHorizon();
+            break;
+        case 2:
+            agregarHalo();
+            break;
+        case 3:
+            vaciarCarrito();
+            break;
+        case 4:
+            consultarCarrito();
+            break;
+        default:
+            alert("El número ingresado es incorrecto, por favor ingrese un número entre 1 y 5")
+            break;
+    }
+    entrada = parseFloat(prompt("Seleccione una de las siguientes opciones: \n1) Agregar Horizzon forbidden west al carrito \n2) Agregar Halo Infinite al carrito \n3) Vaciar carrito \n4) Consultar carrito \n5) Finalizar compra"));
+}
+
+if (carrito == 0){
+    alert("Gracias por visitar nuestra página, esperemos que la próxima vez adquiera alguna copia de nuestros videojuegos.")
+} else {
+    alert(`Su compra finalizo con exito, el valor final de la compra fue de $${carrito}, gracias por visitar nuestra página.`);
+}
